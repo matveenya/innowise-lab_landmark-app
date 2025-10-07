89b2d9d72e795eeb2db2ced72116310eb4e3899b
Roman Matveenya <romanmatveena@gmail.com>
2025-10-07 18:46:42 +0300

    Configure ESlint and Prettier and link it with ESlint,modified package.json

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,11 +2,35 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
+import prettier from "eslint-config-prettier";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { 
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"], 
+    plugins: { js }, 
+    extends: ["js/recommended"], 
+    languageOptions: { 
+      globals: globals.browser 
+    } 
+  },
   tseslint.configs.recommended,
   pluginVue.configs["flat/essential"],
-  { files: ["**/*.vue"], languageOptions: { parserOptions: { parser: tseslint.parser } } },
+  { 
+    files: ["**/*.vue"], 
+    languageOptions: { 
+      parserOptions: { 
+        parser: tseslint.parser 
+      } 
+    } 
+  },
+  prettier,
+  {
+    rules: {
+      'prettier/prettier': 'error',
+      'no-console': 'warn',
+      'no-unused-vars': 'warn',
+      'prefer-const': 'error'
+    }
+  }
 ]);
