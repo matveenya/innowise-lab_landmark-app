30d3a805ae375f294d9182b56cc04474eaba92bf
Roman Matveenya <romanmatveena@gmail.com>
2025-10-14 18:04:23 +0300

    Installing Leaflet, adding landmark interfaces, and updating Firebase configuration

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,3 +3,27 @@ export interface User {
   email: string;
   displayName?: string;
 }
+
+export interface Landmark {
+  id: string;
+  name: string;
+  description: string;
+  latitude: number;
+  longtitude: number;
+  createdBy: string;
+  createdAt: Date;
+  averageRating: number;
+  visiteCount: number;
+  photos: string[];
+  userRatings: { [userId: string]: number };
+}
+
+export interface LandmarkFormData {
+  id: string;
+  name: string;
+  description: string;
+  latitude: number;
+  longtitude: number;
+  userRating: number;
+  photos: File[];
+}
