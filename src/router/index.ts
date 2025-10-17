dfbf23bc32e0c8ec9c6bf3657717bd4e12856913
Roman Matveenya <romanmatveena@gmail.com>
2025-10-17 16:16:06 +0300

    fixed settings for deploy

--- src/router/index.ts
+++ src/router/index.ts
@@ -37,7 +37,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory('/innowise-lab_landmark-app/'),
+  history: createWebHistory(),
   routes,
 });
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'innowise-lab_landmark-app',
   plugins: [vue()],
   server: {
     open: true,
